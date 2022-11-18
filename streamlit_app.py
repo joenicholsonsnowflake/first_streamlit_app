8b6ca7d3b90b53e741a081b0a6e0eae35a46cd32
Joe Nicholson Snowflake <118204520+joenicholsonsnowflake@users.noreply.github.com>
2022-11-18 11:50:40 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # Joe's the GOAT
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
